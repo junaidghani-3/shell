9d880baa81b6fd252b790d26a96cb129820656ee
Junaid Ghani <junaid-demo@users.noreply.github.com>
2025-11-08 16:46:50 -0500

    Final Submission

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -31,31 +31,26 @@ unzip -q rawdata.zip
 mkdir data
 
 # 2. Move the ./rawdata directory to ./data/raw
-mkdir raw
+mkdir ./data/raw
 mv ./rawdata/* ./data/raw
 # 3. List the contents of the ./data/raw directory
-cd ./data/raw
-ls 
+ls ./data/raw
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
-cd ..
-cd processed
-mkdir server_logs user_logs event_logs
+mkdir ./data/processed
+mkdir ./data/processed/server_logs ./data/processed/user_logs  ./data/processed/event_logs
+
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-cd..
-cp *server*.log ./data/processed/server_logs
+cp ./data/raw/*server*.log ./data/processed/server_logs
 # 6. Repeat the above step for user logs and event logs
-cp *user*.log ./data/processed/user_logs
-cp *event*.log ./data/processed/event_logs
+cp ./data/raw/*user*.log ./data/processed/user_logs
+cp ./data/raw/*event*.log ./data/processed/event_logs
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-cd ./data/raw
-rm *ipaddr*
-cd ./data/processed/user_logs
-rm *ipaddr*
+rm ./data/raw/*ipaddr*
+rm ./data/processed/user_logs/*ipaddr*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-cd ../
 touch ./data/inventory.txt
-find ./data/processed -type f > inventory.txt
-cat inventory.txt
+find ./data/processed -type f >> inventory.txt
+cat ./data/inventory.txt
 
 ###########################################
 
