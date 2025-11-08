a0bd2389bb6f809e1d1d409a9cdbd3f27616dda5
Junaid Ghani <junaid-demo@users.noreply.github.com>
2025-11-08 16:11:27 -0500

    Attempt 2: All parts submitted

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -31,7 +31,8 @@ unzip -q rawdata.zip
 mkdir data
 
 # 2. Move the ./rawdata directory to ./data/raw
-cp ./rawdata/* ./data/raw
+mkdir raw
+mv ./rawdata/* ./data/raw
 # 3. List the contents of the ./data/raw directory
 cd ./data/raw
 ls 
